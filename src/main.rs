f1ccc517dbc0fe4a7face2feccfea0911c0087e0
philopaterwaheed <philopaterwaheed9@gmail.com>
2024-04-10 00:00:38 +0000

    pre moving dirs

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,8 @@ fn main() {
     // let x = dirs::Directory::new("/home/philosan/Dev/rust/fileio/src/main.rs");
     let y = dirs::Directory::new("philosan",Path::new("/home/philosan/")).unwrap();
     let mut z = dirs::Directory::new("c#" , Path::new("/home/philosan/Dev/c#/")).unwrap();
-    copy_dir(&z,&y);
-
+    let mut x = files::File::new("hello" , Path::new("/home/philosan/Dev/c#/hello")).unwrap();
+    move_dir(&z,&y);
     // let dirs = dirs::get_dirs(& current_dir).unwrap();
     // for e in dirs{
     //
@@ -71,8 +71,7 @@ fn copy_dir (source_dir : &dirs::Directory ,  dircetion_dir : &dirs::Directory )
 {
     let new_name = source_dir.name.clone ();
     let new_path = &dircetion_dir.path.join(&new_name);
-    // fs::create_dir_all(new_path)?;
-    let dir =     dirs::Directory::new(new_name.as_str(), new_path);
+    let dir = dirs::Directory::new(new_name.as_str(), new_path);
 
 
     for entry in fs::read_dir(source_dir.path.to_owned())? {
@@ -87,11 +86,26 @@ fn copy_dir (source_dir : &dirs::Directory ,  dircetion_dir : &dirs::Directory )
         }
     }
     dir
-
-
-    // fs::copy(source_dir.path.as_path(), dircetion_dir.path.join(new_name.as_str()).as_path())?;
-    // let out_file =  files::File::new(new_name.as_str(),dircetion_dir.path.join(new_name.as_str()).as_path());
-    // out_file
 }
 
+fn move_dir (source_dir : &dirs::Directory ,  dircetion_dir : &dirs::Directory )-> Result<dirs::Directory, io::Error>
+{
+    let new_name = source_dir.name.clone ();
+    let new_path = &dircetion_dir.path.join(&new_name);
+    let dir = dirs::Directory::new(new_name.as_str(), new_path);
+
+
+    for entry in fs::read_dir(source_dir.path.to_owned())? {
+        let entry = entry?;
+            println!("{}",entry.file_name().into_string().ok().unwrap());
+        let ty = entry.file_type()?;
+        if ty.is_dir() {
+            let entry_name = entry.file_name().into_string().ok().unwrap();
+            move_dir(&dirs::Directory::new(entry_name.to_owned().as_str(),entry.path().as_path()).unwrap(),&dirs::Directory::new(entry_name.to_owned().as_str(),new_path.as_path()).unwrap())?;
+        } else {
+            fs::rename(entry.path(), new_path.join(entry.file_name()))?;
+        }
+    }
+    dir
+}
 
