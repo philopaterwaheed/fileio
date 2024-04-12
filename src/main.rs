556ed8692e8f7763c132cb04745c705d25fbec71
philosan <philopaterwaheed9@gmail.com>
2024-04-12 08:38:35 +0200

    added_ui

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,84 @@
 mod Dirs;
 mod Files;
 
-use ncurses::*;
+use::ncurses::*;
+
+use crossterm::{
+    event::{self, Event, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{prelude::*, widgets::*};
 use std::fs;
-use std::io::{self, Write};
+use std::io::{self};
 use std::os::unix::fs::PermissionsExt;
 use std::path::{Path, PathBuf};
 use Dirs::dirs;
 use Files::files;
-fn main() {
-    // Start in the current directory
-    let mut current_dir = std::env::current_dir().expect("Failed to get current directory"); // get the dir
 
-    // let x = dirs::Directory::new("/home/philosan/Dev/rust/fileio/src/main.rs");
-    let y = dirs::Directory::new("philosan", Path::new("/home/philosan/")).unwrap();
-    let mut z = dirs::Directory::new("pop", Path::new("/")).unwrap();
+fn main() -> io::Result<()>{
 
+    enable_raw_mode()?;
+    std::io::stdout().execute(EnterAlternateScreen)?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(std::io::stdout()))?;
+
+    let mut should_quit = false;
+    while !should_quit {
+        terminal.draw(ui)?;
+        should_quit = handle_events()?;
+    }
+
+    disable_raw_mode()?;
+    std::io::stdout().execute(LeaveAlternateScreen)?;
+    Ok(())
+}
+
+fn handle_events() -> io::Result<bool> {
+    if event::poll(std::time::Duration::from_millis(50))? {
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                return Ok(true);
+            }
+        }
+    }
+    Ok(false)
+}
+fn ui(frame: &mut Frame) {
+ let main_layout = Layout::new(
+        Direction::Vertical,
+        [
+            Constraint::Length(1),
+            Constraint::Min(0),
+            Constraint::Ratio(30,100),
+        ],
+    )
+    .split(frame.size());
+    frame.render_widget(
+        Block::new().borders(Borders::TOP).title("Fileio"),
+        main_layout[0],
+    );
+    frame.render_widget(
+        Block::new().borders(Borders::ALL).title("Commands"),
+        main_layout[2],
+    );
+
+    let inner_layout = Layout::new(
+        Direction::Horizontal,
+        [Constraint::Percentage(50), Constraint::Percentage(50),Constraint::Percentage(50)],
+    )
+    .split(main_layout[1]);
+    frame.render_widget(
+        Block::default().borders(Borders::ALL).title("Prev"),
+        inner_layout[0],
+    );
+    frame.render_widget(
+        Block::default().borders(Borders::ALL).title("Curr"),
+        inner_layout[1],
+    );
+    frame.render_widget(
+        Block::default().borders(Borders::ALL).title("Next"),
+        inner_layout[2],
+    );
 }
 
 fn print_files(path: &std::path::PathBuf) { // edit we will just display it on ncurses
