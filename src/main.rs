3df8fb06b7ceb5f213840291337639a8dfccbcbb
philosan <philopaterwaheed9@gmail.com>
2024-04-16 03:15:12 +0200

    added_copy_cut_paste

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,9 @@ fn main() -> io::Result<()>{
     let mut opera_code : usize = 0 ; //the code of the operation the user will do
     let mut input_state = (&mut input_mode, input_string , &mut opera_code);
 
+    let mut buffer_vec : &mut Vec<(Entry, bool)> = &mut Vec::new() ; // the copy and paste buffer
+    let mut buffer_state = (0 as usize, buffer_vec); // hte buffer vector and its selection
+
     enable_raw_mode()?;
     std::io::stdout().execute(EnterAlternateScreen)?;
     let mut terminal = Terminal::new(CrosstermBackend::new(std::io::stdout()))?;
@@ -48,8 +51,8 @@ fn main() -> io::Result<()>{
     let mut should_quit = false;
     update(selections ,contains);
     while !should_quit {
-        terminal.draw(|f|ui(f,selections , contains,&input_state.1.to_owned()))?;
-        should_quit = handle_events(selections, contains,  &mut input_state)?;
+        terminal.draw(|f|ui(f,selections , contains,&input_state.1.to_owned(),&mut buffer_state))?;
+        should_quit = handle_events(selections, contains,  &mut input_state, &mut buffer_state)?;
         unsafe{
             if CLEAR{ // clear before entering sub terminal
                 enable_raw_mode();
@@ -65,7 +68,7 @@ fn main() -> io::Result<()>{
     Ok(())
 }
 
-fn handle_events(selections : &mut (usize , &mut dirs::Directory ,usize ,  Entry), contants  : &mut (Vec<PathBuf>,Vec<String>),input_state: &mut (&mut bool,&mut String , &mut usize)) -> io::Result<bool> {
+fn handle_events(selections : &mut (usize , &mut dirs::Directory ,usize ,  Entry), contants  : &mut (Vec<PathBuf>,Vec<String>),input_state: &mut (&mut bool,&mut String , &mut usize) , buffer_state: &mut (usize,&mut Vec<(Entry, bool)>)) -> io::Result<bool> {
     if event::poll(std::time::Duration::from_millis(50))? {
         if let Event::Key(key) = event::read()? {
             if !*(input_state.0 /* mode of input */ ){ //if the user not trying to input text
@@ -111,6 +114,77 @@ fn handle_events(selections : &mut (usize , &mut dirs::Directory ,usize ,  Entry
                     *input_state.0= true; 
                     *input_state.2= 1; 
                 }
+                if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('y'){ // copy the Entry
+                    let copy_entry : Entry ; 
+                    match & selections.3 { // chciking the type of selected entry
+                        Entry::dir(d)=>{copy_entry = Entry::dir(dirs::Directory::new(d.path.as_path()).unwrap()) },
+                        Entry::file(f)=>{copy_entry = Entry::file(files::File::new(f.path.as_path()).unwrap()) },
+                        Entry::None=>{copy_entry = Entry::None},
+                    }
+                    if let Entry::None = copy_entry{}
+                    else {buffer_state.1.push ((copy_entry,false));}
+                }
+                if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('d'){ // removes the Entry
+                    let cut_entry : Entry ; 
+                    match & selections.3 { // chciking the type of selected entry
+                        Entry::dir(d)=>{cut_entry = Entry::dir(dirs::Directory::new(d.path.as_path()).unwrap()) },
+                        Entry::file(f)=>{cut_entry = Entry::file(files::File::new(f.path.as_path()).unwrap()) },
+                        Entry::None=>{cut_entry = Entry::None},
+                    }
+                    if let Entry::None = cut_entry{}
+                    else {buffer_state.1.push ((cut_entry,true));}
+                }
+                if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('p'){ // pastes buffer the Entry
+                    for  (entry , operation )in  buffer_state.1.iter_mut(){
+                        match &entry { // chciking the type of element
+                                Entry::dir(d)=>{
+
+                                    if *operation{// if we are cutting a dir
+                                        if let Ok(x) =  move_dir(&d, selections.1){
+                                            // if we are not moving into self ; 
+                                        }
+                                        else{
+
+                                        }
+                                    }
+                                    else { // if we are copying
+                                       let _= copy_dir(&d, selections.1);
+                                    }
+                                },
+                                Entry::file(f)=>{
+                                    if *operation {// if we are cutting a dir
+                                        let _= move_file(&f, selections.1);
+                                    }
+                                    else { // if we are copying
+                                       let _= copy_file(&f, selections.1);
+                                    }
+                                },
+                                Entry::None=>{},
+                            }
+                    }
+                    buffer_state.1.clear();//clear the buffer 
+                }
+                if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('w'){ // decreese the buffer selection
+                    if buffer_state.0 > 0{
+                        buffer_state.0 = buffer_state.0 - 1 ;
+                    }
+                }
+                if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('s'){ // increese the buffer selection
+                    if buffer_state.0 <  buffer_state.1.len()-1 { // if we are less than the buffer
+                                                               // size
+                        buffer_state.0 = buffer_state.0 + 1 ;
+                    }
+                }
+                if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('x'){ // delete
+                                                                                             // selection
+                                                                                             // from
+                                                                                             // buffer
+                    if buffer_state.1.len() !=0 && buffer_state.0 < buffer_state.1.len(){
+                        buffer_state.1.remove(buffer_state.0);
+                    }
+                    buffer_state.0 = 0 ; 
+                }
+                                                                                             
             }
             else{ //if we are in input mode
                 if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Enter {
@@ -154,15 +228,26 @@ fn input_operation_excute(selections : &mut (usize , &mut dirs::Directory ,usize
     Ok(())
 }
 
-fn ui(frame: &mut Frame, selections : &mut (usize , &mut dirs::Directory ,usize ,  Entry) , constants  : &mut (Vec<PathBuf>,Vec<String>), input_string: &str) {
+fn ui(frame: &mut Frame, selections : &mut (usize , &mut dirs::Directory ,usize ,  Entry) , constants  : &mut (Vec<PathBuf>,Vec<String>), input_string: &str, buffer_state: &mut (usize,&mut Vec<(Entry, bool)>)) {
     let commands: Vec<String> = vec![
     "('D' , Delete  : remove)".to_string(),
     "('r'   :  rename )".to_string(),
 ];
+ let mut buffer : Vec <String> = Vec::new();
  let curr = &selections.1; // the curr dir
  let prev = curr.prev(); // gets the prev dir
  selections.0 = curr.find_index(); // finds the index of curr in prev 
-
+for  (entry , _operation )in  buffer_state.1.iter_mut(){ // getting the buffer as a vec of string
+    match &entry { // chciking the type of element
+            Entry::dir(d)=>{
+                buffer.push(d.name.to_owned());
+            },
+            Entry::file(f)=>{
+                buffer.push(f.name.to_owned());
+            },
+            Entry::None=>{},
+        }
+}
  let (current_contains_pathes,current_contains_strings)= constants; // the contains of curr as vec
                                                                     // of string and PathBuf
  let main_layout = Layout::new(
@@ -189,8 +274,10 @@ fn ui(frame: &mut Frame, selections : &mut (usize , &mut dirs::Directory ,usize 
     let mut sel = ListState::default(); // selection state of curr dir
     let mut no_sel = ListState::default();// selection state of next dir just set to nothing 
     let mut prev_sel = ListState::default();// selection state of prev dir
+    let mut buffer_sel = ListState::default();// selection state of buffer
     sel.select(Some(selections.2)); // Select the first item initially
     prev_sel.select(Some(selections.0)); // Select the index of curr in prev
+    buffer_sel.select(Some(buffer_state.0)); // Selection of buffer
     if let Ok(d) = prev  { // checks if the prev dir exists in case we are in / 
         let prev_c = d.vec_of_contains().unwrap().1;
         render_list(frame, inner_layout[0],prev_c , &mut prev_sel,"Prev"); // renders it 
@@ -200,7 +287,6 @@ fn ui(frame: &mut Frame, selections : &mut (usize , &mut dirs::Directory ,usize 
      if let Entry::dir(d) = &selections.3 { // if the selection is on a dir 
              let next_c = d.vec_of_contains().unwrap().1;
              render_list(frame, inner_layout[2],next_c , &mut no_sel,"next");
-
      }
 
     // the operations area
@@ -222,6 +308,7 @@ fn ui(frame: &mut Frame, selections : &mut (usize , &mut dirs::Directory ,usize 
     )
     .split(down_layout[1]);
     render_list(frame, operation_layout[0] ,commands , &mut no_sel,"command");
+    render_list(frame, operation_layout[1] ,buffer, &mut buffer_sel,"buffer");// hte buffer
 
 }
 
@@ -266,7 +353,7 @@ fn render_list (frame : &mut Frame ,rect : Rect ,contains : Vec<String>,state:&m
         rect,state
      );
 }
-fn copy_file(file: files::File, dircetion_dir: &dirs::Directory) -> Result<files::File, io::Error> {
+fn copy_file(file: &files::File, dircetion_dir: &dirs::Directory) -> Result<files::File, io::Error> {
     let new_name = file.name.clone();
     fs::copy(
         file.path.as_path(),
@@ -277,7 +364,7 @@ fn copy_file(file: files::File, dircetion_dir: &dirs::Directory) -> Result<files
     );
     out_file
 }
-fn move_file(file: files::File, dircetion_dir: &dirs::Directory) -> Result<files::File, io::Error> {
+fn move_file(file: &files::File, dircetion_dir: &dirs::Directory) -> Result<files::File, io::Error> {
     let new_name = file.name.clone();
     fs::rename(
         file.path.as_path(),
@@ -314,24 +401,30 @@ fn copy_dir(
     dir
 }
 
-fn move_dir(source_dir: dirs::Directory, dircetion_dir: &dirs::Directory)-> Result<dirs::Directory, io::Error> {
+fn move_dir(source_dir: &dirs::Directory, dircetion_dir: &dirs::Directory)-> Result<dirs::Directory, io::Error> {
     let new_name = source_dir.name.clone();
     let new_path = &dircetion_dir.path.join(&new_name);
-    let dir = dirs::Directory::new(new_path);
+    if dircetion_dir.path.as_path() != source_dir.path.as_path(){ 
+        let dir = dirs::Directory::new(new_path);
 
-    for entry in fs::read_dir(source_dir.path.to_owned())? {
-        let entry = entry?;
-        let ty = entry.file_type()?;
-        if ty.is_dir() {
-            move_dir(
-                 dirs::Directory::new(entry.path().as_path())
-                    .unwrap(),
-                &dirs::Directory::new(new_path.as_path()).unwrap(),
-            )?;
-        } else {
-            fs::rename(entry.path(), new_path.join(entry.file_name()))?;
+        for entry in fs::read_dir(source_dir.path.to_owned())? {
+            let entry = entry?;
+            let ty = entry.file_type()?;
+            if ty.is_dir() {
+                move_dir(
+                     &dirs::Directory::new(entry.path().as_path())
+                        .unwrap(),
+                    &dirs::Directory::new(new_path.as_path()).unwrap(),
+                )?;
+            } else {
+                fs::rename(entry.path(), new_path.join(entry.file_name()))?;
+            }
         }
+        let _remove_pross = source_dir.remove();
+        dir
     }
-    let _remove_pross = source_dir.remove();
-    dir
+    else {
+        let error_message = format!("move into self" );
+        Err(io::Error::new(io::ErrorKind::NotFound, error_message))
+    }
 }
